1755bfc2dd9eed62ce27c96af5afa51ae4262c00
Clarice Tran <tran.clarice@gmail.com>
2023-02-28 19:39:52 -0800

    Defined the schemas for User and Thought. Also, created reaction subdocument in Thought. Created virtuals to get counts of friends and reactions in User and Thought respectively

--- models/Thought.js
+++ models/Thought.js
@@ -1,3 +1,49 @@
 const { Schema, model } = require("mongoose");
+const dayjs = require("dayjs");
+
+const reactionSchema = new Schema({
+    reactionId: {
+        type: ObjectId,
+        default: new ObjectId(),
+    },
+    reactionBody: {
+        type: String,
+        required: true,
+        maxLength: 280,
+    },
+    username: {
+        type: String,
+        required: true,
+    },
+    createdAt: {
+        type: Date,
+        default: dayjs(),
+        get: (v) => `${dayjs(v).format("MMM DD, YYY at hh:mm a")}`,
+    },
+});
+
+const thoughtSchema = new Schema({
+    thoughtText: {
+        type: String,
+        required: true,
+        minLength: 1,
+        maxLength: 280,
+    },
+    createdAt: {
+        type: Date,
+        default: dayjs(),
+        get: (v) => `${dayjs(v).format("MMM DD, YYY at hh:mm a")}`,
+    },
+    username: {
+        type: String,
+        required: true,
+    },
+    reactions: [reactionSchema],
+});
+
+userSchema.virtual("reactionCount").get(function () {
+    console.log(`Reaction count: ${this.reactions.length}`);
+    return this.reactions.length;
+});
 
 module.exports = Thought;
--- models/User.js
+++ models/User.js
@@ -1,3 +1,30 @@
 const { Schema, model } = require("mongoose");
 
+const userSchema = new Schema({
+    username: {
+        type: String,
+        unique: true,
+        required: true,
+        trim: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        validate: {
+            validator: function (v) {
+                return /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/.test(v);
+            },
+            message: (props) => `${props.value} is not a valid email.`,
+        },
+    },
+    thoughts: [{ type: ObjectId, ref: "Thought" }],
+    friends: [{ type: ObjectId, ref: "User" }],
+});
+
+userSchema.virtual("friendCount").get(function () {
+    console.log(`Friend count: ${this.friends.length}`);
+    return this.friends.length;
+});
+
 module.exports = User;
